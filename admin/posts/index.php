d3b1b578c6ef29bdb54bd307426595f7748bac71
Axel <axel.giraudeau@yahoo.com>
2021-12-14 10:20:06 +0100

    update: admin interface

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -9,41 +9,18 @@ $posts = getPosts();
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template-parts/layout/admin/header.php';
 ?>
 
-    <a href="/admin/posts/create.php" class="btn btn-outline-primary mt-2 float-end">Ajouter une publication</a>
-
     <main id="main">
-
-        <table class="table table-hover table-bordered">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Titre</th>
-                <th>Auteur</th>
-                <th>Date de publication</th>
-                <th>Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($posts as $post): ?>
-            <?php $author = getPostAuthor($post); ?>
-                <tr>
-                    <td><?php echo $post['id']; ?></td>
-                    <td>
-                        <a href="/admin/posts/edit.php?id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a>
-                    </td>
-                    <td><?php echo getValue($author['username']) ? $author['username'] : '-'; ?></td>
-                    <td><?php echo $post['published_at']; ?></td>
-                    <td>
-                        <a href="/admin/posts/edit.php?id=<?php echo $post['id']; ?>"
-                           class="btn btn-sm btn-outline-warning">Edit</a>
-                        <a href="/posts/show.php?id=<?php echo $post['id']; ?>&preview=true"
-                           class="btn btn-sm btn-outline-info">Preview</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-            </tbody>
-        </table>
-
+        <div class="container py-5">
+            <div class="posts-wrapper">
+                <div class="row g-4">
+                    <?php foreach ($posts as $post): ?>
+                        <div class="col-md-6 col-lg-4">
+                            <?php require $_SERVER['DOCUMENT_ROOT'] . '/template-parts/post.php'; ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
     </main>
 
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/template-parts/layout/admin/footer.php'; ?>
