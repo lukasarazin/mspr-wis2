47d4d6c6e8bb389a66b3a607bd726751149b2efe
Axel <axel.giraudeau@yahoo.com>
2021-12-15 16:18:38 +0100

    update: style login + register

--- login.php
+++ login.php
@@ -5,14 +5,13 @@ require_once 'template-parts/layout/header.php';
 
 middleware('guest');
 ?>
-
     <main id="main">
 
         <section id="auth-content">
             <div class="container-fluid">
                 <!-- j'ai tenté de changer l'apparence des messages d'erreurs (qui surviendraient en cas de mauvais mot de passe par exemple)
                 en utilisant celle de Bootstrap, malheureusement, la fonction que j'avais créé n'a pas marché :( -->
-                <div class="connexion card mx-auto shadow rounded-3" style="max-width: 450px; margin-top: 150px;">
+                <div class="connexion card mx-auto shadow rounded-3" style="max-width: 450px; margin-top: 8rem;">
 
                     <div class="card-header">
                         <h1 class="h2 mb-0">Connexion</h1>
@@ -22,30 +21,27 @@ middleware('guest');
 
                             <div class="mb-3">
                                 <label class="form-label" for="email">Email</label>
-                                <input type="text" class="form-control rounded-3" name="email" id="email"
+                                <input type="text" class="form-control" name="email" id="email"
                                        maxlength="255"
                                        required>
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label" for="password">Mot de passe</label>
-                                <input type="password" class="form-control rounded-3" name="password" id="password"
+                                <input type="password" class="form-control" name="password" id="password"
                                        maxlength="255" minlength="6" required>
                             </div>
 
                             <div class="card-content">
-
-
-
                                 <div class="mb-3 lost-mdp">
                                     <a href="">Mot de passe oublié ?</a>
                                 </div>
                             </div>
-                                <div class="d-grid gap-2">
-                                    <button type="submit" class="btn btn-primary rounded-3 ">
-                                        Connexion
-                                    </button>
-                                </div>
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn-log rounded-3">
+                                    Connexion
+                                </button>
+                            </div>
 
                         </form>
                     </div>
--- register.php
+++ register.php
@@ -7,59 +7,59 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/template-parts/layout/header.php';
 middleware('guest');
 ?>
 
-<main id="main">
+    <main id="main">
 
-    <section id="auth-content">
-        <div class="container">
+        <section id="auth-content">
+            <div class="container">
 
-            <div class="card mx-auto shadow" style="max-width: 600px; margin-top: 200px">
+                <div class="card mx-auto shadow rounded-3" style="max-width: 450px; margin-top: 8rem">
 
-                <div class="card-header">
-                    <h1 class="h2 mb-0">Inscription</h1>
+                    <div class="card-header">
+                        <h1 class="h2 mb-0">Inscription</h1>
+                    </div>
+                    <div class="card-body py-4">
+                        <form action="api/auth/register.php" method="POST">
+
+                            <div class="mb-3">
+                                <label class="form-label" for="username">Nom d'utilisateur</label>
+                                <input type="text" class="form-control" name="username" id="username"
+                                       maxlength="255"
+                                       required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label" for="email">Email</label>
+                                <input type="text" class="form-control" name="email" id="email" maxlength="255"
+                                       required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label" for="password">Mot de passe</label>
+                                <input type="password" class="form-control" name="password" id="password"
+                                       maxlength="255" minlength="6" required>
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label" for="password_confirm">Confirmation du mot de
+                                    passe</label>
+                                <input type="password" class="form-control" name="password_confirm"
+                                       id="password_confirm" maxlength="255" minlength="6"
+                                       required>
+                            </div>
+
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn-log rounded-3">
+                                    Inscription
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <form action="api/auth/register.php" method="POST">
 
-                        <div class="mb-3">
-                            <label class="form-label" for="username">Nom d'utilisateur</label>
-                            <input type="text" class="form-control" name="username" id="username"
-                                   maxlength="255"
-                                   required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label class="form-label" for="email">Email</label>
-                            <input type="text" class="form-control" name="email" id="email" maxlength="255"
-                                   required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label class="form-label" for="password">Mot de passe</label>
-                            <input type="password" class="form-control" name="password" id="password"
-                                   maxlength="255" minlength="6" required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label class="form-label" for="password_confirm">Confirmation du mot de
-                                passe</label>
-                            <input type="password" class="form-control" name="password_confirm"
-                                   id="password_confirm" maxlength="255" minlength="6"
-                                   required>
-                        </div>
-
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-outline-primary">
-                                Inscription
-                            </button>
-                        </div>
-
-                    </form>
-                </div>
             </div>
+        </section>
 
-        </div>
-    </section>
-
-</main>
+    </main>
 
 <?php require_once 'template-parts/layout/footer.php'; ?>
