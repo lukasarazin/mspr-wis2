4e9eabfd664320116446cdc141b79d7596f188ad
Axel <axel.giraudeau@yahoo.com>
2021-12-16 15:20:37 +0100

    update: yeux sur password register

--- register.php
+++ register.php
@@ -12,6 +12,12 @@ middleware('guest');
         <section id="auth-content">
             <div class="container">
 
+                <?php if (getValue($_GET['error'])): ?>
+                    <div id="alert" class="alert alert-danger" role="alert">
+                        <p class="mb-0">Le mot de passe ne correspond pas à la confirmation</p>
+                    </div>
+                <?php endif; ?>
+
                 <div class="connexion card mx-auto shadow rounded-3">
 
                     <div class="card-header">
@@ -35,20 +41,34 @@ middleware('guest');
 
                             <div class="mb-3">
                                 <label class="form-label" for="password">Mot de passe</label>
-                                <input type="password" class="form-control" name="password" id="password"
-                                       maxlength="255" minlength="6" required>
+                                <div class="input-group">
+                                    <input type="password" class="form-control" name="password" id="password"
+                                           maxlength="255" minlength="6" required>
+                                    <button type="button" class="input-group-text btn btn-sm btn-outline-secondary"
+                                            id="toggle-password">
+                                        <img src="assets/img/open.png" class="eye eye-open" alt="oeil-ouvert">
+                                        <img src="assets/img/close.png" class="eye eye-close" alt="oeil-fermé">
+                                    </button>
+                                </div>
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label" for="password_confirm">Confirmation du mot de
                                     passe</label>
-                                <input type="password" class="form-control" name="password_confirm"
-                                       id="password_confirm" maxlength="255" minlength="6"
-                                       required>
+                                <div class="input-group">
+                                    <input type="password" class="form-control" name="password_confirm"
+                                           id="password_confirm" maxlength="255" minlength="6"
+                                           required>
+                                    <button type="button" class="input-group-text btn btn-sm btn-outline-secondary"
+                                            id="toggle-password-confirm">
+                                        <img src="assets/img/open.png" class="eye eye-open" alt="oeil-ouvert">
+                                        <img src="assets/img/close.png" class="eye eye-close" alt="oeil-fermé">
+                                    </button>
+                                </div>
                             </div>
 
                             <div class="d-grid gap-2">
-                                <button type="submit" class="btn-log rounded-3">
+                                <button id="register-submit" type="submit" class="btn-log rounded-3">
                                     Inscription
                                 </button>
                             </div>
@@ -68,4 +88,38 @@ middleware('guest');
 
     </main>
 
-<?php require_once 'template-parts/layout/footer.php'; ?>
+    <script src="src/js/alert.js"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+
+            let input = document.getElementById('password');
+            let button = document.getElementById('toggle-password');
+
+            button.addEventListener('click', function () {
+                button.classList.toggle('active');
+                if (input.type === 'password') {
+                    input.type = 'text';
+                } else {
+                    input.type = 'password'
+                }
+            });
+
+        })
+
+        document.addEventListener('DOMContentLoaded', function () {
+            let inputConfirm = document.getElementById('password_confirm');
+            let buttonConfirm = document.getElementById('toggle-password-confirm');
+
+            buttonConfirm.addEventListener('click', function () {
+                buttonConfirm.classList.toggle('active');
+                if (inputConfirm.type === 'password') {
+                    inputConfirm.type = 'text';
+                } else {
+                    inputConfirm.type = 'password'
+                }
+            });
+
+        })
+    </script>
+
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/template-parts/layout/footer.php'; ?>
---
+++ src/js/alert.js
@@ -0,0 +1,24 @@
+/*
+function validatePassword() {
+    let passwordInput = document.getElementById('password')
+    let passwordConfirmInput = document.getElementById('password_confirm')
+    let password = passwordInput.value
+    let passwordConfirm = passwordConfirmInput.value
+
+    if (!password.length || !passwordConfirm.length) {
+        console.error('Champs vides');
+    }
+
+    if (password !== passwordConfirm) {
+        let alert = document.getElementById('alert');
+        alert.hidden = false;
+
+        setTimeout(function () {
+            alert.hidden = true;
+        }, 4000);
+    }
+}
+
+let button = document.getElementById('register-submit');
+button.addEventListener('click', validatePassword);
+*/
