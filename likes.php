2a3bdecfb08e48e721fc936c98c121c3dfe5b829
Luka Sarazin <sarazinluka@gmail.com>
2021-12-16 14:52:03 +0100

    update: follow

--- likes.php
+++ likes.php
@@ -15,32 +15,32 @@ $auth = getAuth();
 $data = ['user_id' => $auth['id']];
 $likes = getLikes($data);
 
-var_dump($likes);
 require_once $_SERVER['DOCUMENT_ROOT'] . '/template-parts/layout/header.php';
 
 ?>
 
-<section class="posts-wrapper">
+<article class="posts-wrapper">
     <div class="container">
         <div class="row g-4">
 
             <?php foreach ($likes as $like): ?>
-                <div class="col-md-6 col-lg-4">
-                    <?php var_dump (getPost($like['post_id'])) ?>
-                    <div class="profil-img">
-                        <a href="/users/show.php?id=<?php echo $user['id']; ?>">
-                            <img class="user-img rounded-circle"
-                                 src="<?php echo getAvatarUrl($user['email']); ?>"
-                                 alt="Photo de profil"
-                                 title="Photo de profil"
-                                 loading="lazy"
-                                 width="30">
-                        </a>
-                    </div>
+                <?php var_dump($like); ?>
+                <div class="profil-img">
+                    <a href="/users/show.php?id=<?php echo $user['id']; ?>">
+                        <img class="user-img rounded-circle"
+                             src="<?php echo getAvatarUrl($user['email']); ?>"
+                             alt="Photo de profil"
+                             title="Photo de profil"
+                             loading="lazy"
+                             width="30">
+                    </a>
                 </div>
+
+
+
             <?php endforeach; ?>
 
 
         </div>
     </div>
-</section>
+</article>
