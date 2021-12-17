3dd7f82f58d552bd37e4723bc249e485423d7ba0
Axel <axel.giraudeau@yahoo.com>
2021-12-17 13:14:09 +0100

    update: style template/search.php

--- template-parts/search.php
+++ template-parts/search.php
@@ -11,8 +11,8 @@ require_once '../template-parts/layout/header.php';
 ?>
     <main id="main">
         <section id="search-users">
-            <div class="container mt-5">
-                .
+            <div class="search-results-container container mt-5">
+
                 <h1>Recherche pour : <?php echo $_GET['search'] ?></h1>
 
                 <span scope="col" style="color: lightgrey">Résultats </span>
@@ -20,19 +20,21 @@ require_once '../template-parts/layout/header.php';
 
                     <?php foreach ($users as $user): ?>
                 <div class="mt-5 d-flex">
-                        <img class="rounded-circle mx-3" src="<?php echo $user['avatar']; ?>"
-                             alt="Photo de <?php echo $user['username']; ?>"
-                             title="Photo de <?php echo $user['username']; ?>"
-                             width="65"
-                             height="65"
-                             loading="lazy">
-                        <div>
-                            <a style="color: white; text-decoration: none; font-size: 25px;"
-                               href="/users/show.php?id=<?php echo $user['id'] ?>">
-                                @<?php echo $user['username'] ?>
-                            </a>
+                    <?php foreach ($users as $user): ?>
+                        <div class="search-result-card">
+                            <img class="rounded-circle mx-3" src="<?php echo $user['avatar']; ?>"
+                                 alt="Photo de <?php echo $user['username']; ?>"
+                                 title="Photo de <?php echo $user['username']; ?>"
+                                 width="65"
+                                 height="65"
+                                 loading="lazy">
                             <div>
-                                <?php echo $user['email']; ?>
+                                <a href="/users/show.php?id=<?php echo $user['id'] ?>">
+                                    @<?php echo $user['username'] ?>
+                                </a>
+                                <div>
+                                    <?php echo $user['email']; ?>
+                                </div>
                             </div>
                         </div>
                         </div>
